0c2473847b9acc115e2eff3a8fcbe5a03b37308a
lenovo <farah_nawar@hotmail.com>
2024-02-14 13:47:32 +0200

    Marker for user's location

--- lib/map_sample.dart
+++ lib/map_sample.dart
@@ -5,26 +5,25 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class MapSample extends StatefulWidget {
-  static const String routeName='map';
+  static const String routeName = 'map';
 
   @override
   State<MapSample> createState() => _MapSampleState();
 }
 
 class _MapSampleState extends State<MapSample> {
-
-  Location location=Location();
+  Location location = Location();
 
   late PermissionStatus permissionStatus;
 
-  bool serviceEnabled= false;
+  bool serviceEnabled = false;
 
   late LocationData? locationData;
 
   StreamSubscription<LocationData>? streamSubscription = null;
 
   final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
+      Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(31.2326654, 29.9458551),
@@ -36,9 +35,9 @@ class _MapSampleState extends State<MapSample> {
       target: LatLng(31.2326654, 29.9458551),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
-  Set<Marker> markers={};
-  double defLat=37.4219983;
-  double defLon=-122.084;
+  Set<Marker> markers = {};
+  double defLat = 37.4219983;
+  double defLon = -122.084;
 
   @override
   void initState() {
@@ -50,18 +49,19 @@ class _MapSampleState extends State<MapSample> {
       position: LatLng(31.2326654, 29.9458551),
     );
     markers.add(usermarker);
-
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     streamSubscription?.cancel();
   }
+
   @override
   Widget build(BuildContext context) {
     getUserLocation();
-    return  Scaffold(
+    return Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
@@ -75,45 +75,60 @@ class _MapSampleState extends State<MapSample> {
         label: const Text('To the lake!'),
         icon: const Icon(Icons.directions_boat),
       ),
-    );;
+    );
+    ;
   }
+
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 
   Future<bool> isPermissionGranted() async {
-    permissionStatus= await location.hasPermission();
-    if(permissionStatus == PermissionStatus.denied){
-      permissionStatus= await location.requestPermission();
+    permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
     }
     return permissionStatus == PermissionStatus.granted;
   }
 
-  Future<bool> isServiceEnabled() async{
-    serviceEnabled=await  location.serviceEnabled();
-    if(serviceEnabled == false){
+  Future<bool> isServiceEnabled() async {
+    serviceEnabled = await location.serviceEnabled();
+    if (serviceEnabled == false) {
       serviceEnabled = await location.requestService();
     }
     return serviceEnabled;
   }
 
-  void getUserLocation() async{
-    var permissinGranted= await isPermissionGranted();
-    if(permissinGranted==false) return; // user denied
+  void getUserLocation() async {
+    var permissinGranted = await isPermissionGranted();
+    if (permissinGranted == false) return; // user denied
     var gpsEnabled = await isServiceEnabled();
-    if(gpsEnabled == false) return;
-    if(gpsEnabled && permissinGranted){
-      locationData= await location.getLocation();
-      streamSubscription= location.onLocationChanged.listen((newLocationData) {  // tracking user location
-
-          locationData = newLocationData;
-
-
-        print('${newLocationData.latitude ?? 0} and ${newLocationData.longitude?? 0}'  );
-
+    if (gpsEnabled == false) return;
+    if (gpsEnabled && permissinGranted) {
+      locationData = await location.getLocation();
+      streamSubscription = location.onLocationChanged.listen((newLocationData) {
+        // tracking user location
+        locationData = newLocationData;
+        updateUserMarker();
+        print(
+            '${locationData?.latitude ?? 0} and ${locationData?.longitude ?? 0}');
       });
     }
+  }
 
+  void updateUserMarker() async {
+    var usermarker = Marker(
+      markerId: MarkerId('user location'),
+      position: LatLng(locationData?.latitude ?? defLat,
+          locationData?.longitude ?? defLon),
+    );
+    markers.add(usermarker);
+    setState(() {});
+    var newCameraPosition = CameraPosition(
+        target: LatLng(locationData?.latitude ?? defLat,
+            locationData?.longitude ?? defLon), zoom: 19);
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(newCameraPosition));
   }
 }
